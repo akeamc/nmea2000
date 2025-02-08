da07da6b92dabb3c3f22fd7aed1d87e8f7f6ac3c
akeamc <ake@amcoff.net>
2025-02-09 00:31:47 +0100

    update MessageBuf

---
+++ README.md
@@ -0,0 +1,3 @@
+# NMEA 2000
+
+A minimal implementation of fast packets as well as some NMEA 2000 fields.
--- src/buf.rs
+++ src/buf.rs
@@ -1,5 +1,15 @@
 pub trait MessageBuf {
+    fn get_u8(&mut self) -> u8;
+
+    fn get_i8(&mut self) -> i8;
+
+    fn get_u16(&mut self) -> u16;
+
     fn get_i16(&mut self) -> i16;
+
+    fn get_u32(&mut self) -> u32;
+
+    fn get_i32(&mut self) -> i32;
 
     /// Get a floating point number from a fixed point 2-byte representation.
     ///
@@ -22,7 +32,32 @@ where
     T: bytes::Buf,
 {
     #[inline]
+    fn get_u8(&mut self) -> u8 {
+        self.get_u8()
+    }
+
+    #[inline]
+    fn get_i8(&mut self) -> i8 {
+        self.get_i8()
+    }
+
+    #[inline]
+    fn get_u16(&mut self) -> u16 {
+        self.get_u16_le()
+    }
+
+    #[inline]
     fn get_i16(&mut self) -> i16 {
         self.get_i16_le()
+    }
+
+    #[inline]
+    fn get_u32(&mut self) -> u32 {
+        self.get_u32_le()
+    }
+
+    #[inline]
+    fn get_i32(&mut self) -> i32 {
+        self.get_i32_le()
     }
 }
